320409ee7430bed9f781f4ec65a8d19f920042de
Faktor 10 <oreillyross@gmail.com>
2020-02-12 14:54:39 +0100

    add hasura

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import styled from "styled-components";
 import Button from "@material-ui/core/Button";
 import { ArticleTable } from "./ArticleTable";
 import { Router, Link } from "@reach/router";
-import Toggle from 'react-toggle'
-import "react-toggle/style.css" 
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+// the Apollo cache is set up automatically
+const client = new ApolloClient({
+  uri: "https://horizon-me.herokuapp.com/v1/graphql",
+  fetchOptions: {
+    mode: "cors"
+  }
+});
+
+const ARTICLES = gql`
+  {
+    Settings {
+      AutomatedSearch
+    }
+  }
+`;
 
 const StyledDashboard = styled.div`
   display: grid;
@@ -23,52 +43,62 @@ const StyledNavigation = styled.div`
 
 const StyledToggleOption = styled.div`
   display: grid;
-grid-template-columns: 1fr 1fr 
-`
+  grid-template-columns: 1fr 1fr;
+`;
 
 function NotFound() {
   return <div>Not found</div>;
 }
 
 function Admin() {
-  return (<div><h3>Admin screen</h3>
-   <StyledToggleOption>
-    <span>Automated Search on</span>
-  <Toggle
-    icons={false}
-    />
-  </StyledToggleOption>
+  const { error, loading, data } = useQuery(ARTICLES);
+  if (loading) return <div> Loading</div>;
+  if (error) return <div>Error </div>;
+  if (data) {
+    return (
+      <div>
+        <h3>Admin screen</h3>
 
-  </div>);
+        <StyledToggleOption>
+          <span>Automated Search on</span>
+          <Toggle
+            defaultChecked={data.Settings[0].AutomatedSearch}
+            icons={false}
+          />
+        </StyledToggleOption>
+      </div>
+    );
+  }
+}
+
+function Home() {
+  return <div>Welcome</div>;
 }
 
 export default function App() {
   return (
-    <StyledDashboard>
-      <StyledHeader>Horizon Analyst Dashboard</StyledHeader>
-      <StyledNavigation>
-        <Link to="/">
-          <Button>Home</Button>
-        </Link>
-        <Link to="/admin">
-          <Button>Admin</Button>
-        </Link>
-        <Link to="/articles">
-          <Button>Articles</Button>
-        </Link>
-      </StyledNavigation>
-      <Router>
-        <Home path="/" />
+    <ApolloProvider client={client}>
+      <StyledDashboard>
+        <StyledHeader>Horizon Analyst Dashboard</StyledHeader>
+        <StyledNavigation>
+          <Link to="/">
+            <Button>Home</Button>
+          </Link>
+          <Link to="/admin">
+            <Button>Admin</Button>
+          </Link>
+          <Link to="/articles">
+            <Button>Articles</Button>
+          </Link>
+        </StyledNavigation>
+        <Router>
+          <Home path="/" />
 
-        <ArticleTable path="/articles" />
-        <Admin path="/admin" />
-        <NotFound default />
-      </Router>
-    </StyledDashboard>
+          <ArticleTable path="/articles" />
+          <Admin path="/admin" />
+          <NotFound default />
+        </Router>
+      </StyledDashboard>
+    </ApolloProvider>
   );
 }
-const Home = () => (
-  <div>
-    <h2>Welcome</h2>
-  </div>
-);
