2282ab04114e8688241233d9d2310969ba8aba0a
Faktor 10 <oreillyross@gmail.com>
2020-02-19 15:20:19 +0100

    more form stuff

--- src/forms/SourceForm.js
+++ src/forms/SourceForm.js
@@ -1,37 +1,93 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import styled from 'styled-components'
+import React from "react";
+import { useFormik } from "formik";
+import styled from "styled-components";
+import {
+  TextField,
+  InputLabel,
+  Input,
+  Button,
+  FormLabel,
+  Paper
+} from "@material-ui/core";
+//import "../style.css";
 
-const StyledSourceForm = styled.div`
-  margin:1.2rem;
-`
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 400
+    }
+  }
+}));
 
 const SourceForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
+  const classes = useStyles();
+
   const formik = useFormik({
     initialValues: {
-      email: '',
+      sourceName: "",
+      sourceDescription: "",
+      sourceUrl: ""
     },
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
-    },
+    }
   });
   return (
-    <StyledSourceForm>
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type="submit">Submit</button>
-    </form>
-    </StyledSourceForm>
+    <Paper>
+      <FormLabel className="form-label"> Source Form</FormLabel>
+      <form
+        className={classes.root}
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+      >
+        <div>
+          <InputLabel>Name</InputLabel>
+          <TextField
+            id="sourceName"
+            name="sourceName"
+            variant="outlined"
+            required
+            label="Name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.sourceName}
+          />
+        </div>
+        <div>
+          <TextField
+            id="sourceDescription"
+            name="sourceDescription"
+            multiline
+            label="Description"
+            variant="outlined"
+            fullWidth
+            onChange={formik.handleChange}
+            value={formik.values.sourceDescription}
+          />
+        </div>
+        <div>
+          <TextField
+            id="sourceUrl"
+            name="sourceUrl"
+            variant="outlined"
+            label="Hyperlink"
+            type="url"
+            onChange={formik.handleChange}
+            value={formik.values.sourceUrl}
+          />
+        </div>
+        <div style={{ paddingRight: ".8rem", textAlign: "right" }}>
+          <Button color="secondary  ">Cancel</Button>
+          <Button color="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </Paper>
   );
 };
 
-export default SourceForm
+export default SourceForm;
