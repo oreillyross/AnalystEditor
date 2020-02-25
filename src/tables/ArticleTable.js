86f8646441742536b2230c315f497502bf0a4014
Faktor 10 <oreillyross@gmail.com>
2020-02-25 08:41:19 +0100

    pull Articles out into seperate component

--- src/tables/ArticleTable.js
+++ src/tables/ArticleTable.js
@@ -1,21 +1,7 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
-import Article from "../views/Article";
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650
-  }
-});
+import Articles from '../views/Articles'
 
 const GET_ARTICLES = gql`
   query getArticles {
@@ -32,44 +18,13 @@ const GET_ARTICLES = gql`
 `;
 
 export function ArticleTable() {
-  const classes = useStyles();
+
   const { loading, error, data } = useQuery(GET_ARTICLES);
 
   if (loading) return null;
   if (error) return <div>Oops</div>;
 
   if (data) {
-    return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Article</TableCell>
-              <TableCell align="left">Source</TableCell>
-              <TableCell align="right">Tags</TableCell>
-              <TableCell align="right">Indications</TableCell>
-              <TableCell align="right">Scenarios</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.Articles.map(row => (
-              <TableRow key={row.title}>
-                <TableCell>
-                  <a href={`/article/${row.id}`}>{row.title}</a>
-                </TableCell>
-                <TableCell align="left">
-                  {row.Article_Source_Link.name}
-                </TableCell>
-                <TableCell align="right" />
-                <TableCell align="right">
-                  {row.ArticleIndicationCount}
-                </TableCell>
-                <TableCell align="right">{row.articleScenarioCount}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+    return <Articles articles={data.Articles}/>
   }
 }
---
+++ src/views/Articles.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Article from "../views/Article";
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
+
+function Articles({articles}) {
+  
+  const classes = useStyles();
+  
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Article</TableCell>
+            <TableCell align="left">Source</TableCell>
+            <TableCell align="right">Tags</TableCell>
+            <TableCell align="right">Indications</TableCell>
+            <TableCell align="right">Scenarios</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {articles.map(row => (
+            <TableRow key={row.title}>
+              <TableCell>
+                <a href={`/article/${row.id}`}>{row.title}</a>
+              </TableCell>
+              <TableCell align="left">
+                {row.Article_Source_Link.name}
+              </TableCell>
+              <TableCell align="right" />
+              <TableCell align="right">
+                {row.ArticleIndicationCount}
+              </TableCell>
+              <TableCell align="right">{row.articleScenarioCount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default Articles
