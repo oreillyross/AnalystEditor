d83285a00df1c9b28ef959927a9354e05a6d3634
Faktor 10 <oreillyross@gmail.com>
2020-02-27 10:37:19 +0100

    searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Input } from "@material-ui/core";
+import { OutlinedInput } from '@material-ui/core';
 
 
-function SearchBar({ placeholder = "Search" }) {
+function GenericSearchBar({ placeholder = "Search" }) {
   
  return (
     <div>
-      <Input placeholder={placeholder} />
+      <OutlinedInput placeholder={placeholder} fullWidth/>
     </div>
   );
 }
 
-export default SearchBar;
+export default GenericSearchBar;
