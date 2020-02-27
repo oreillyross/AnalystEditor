6e0a90462e7ac6c0c3e9090105f617f9009a7c0c
Faktor 10 <oreillyross@gmail.com>
2020-02-27 11:06:52 +0100

    use styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { OutlinedInput } from '@material-ui/core';
+import { OutlinedInput } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  input: {
+    color: "red",
+    padding: 0
+  }
+});
 
 function GenericSearchBar({ placeholder = "Search" }) {
-  
- return (
+  const classes = useStyles();
+  return (
     <div>
-      <OutlinedInput placeholder={placeholder} fullWidth/>
+      <OutlinedInput
+        className={classes.input}
+        placeholder={placeholder}
+        fullWidth
+      />
     </div>
   );
 }
