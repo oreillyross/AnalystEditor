b42f51df5a2b7417fcb56bc40183f3ad7b4ed363
Faktor 10 <oreillyross@gmail.com>
2020-03-02 11:23:11 +0100

    select text working

--- src/components/ArticleTextSelect.js
+++ src/components/ArticleTextSelect.js
@@ -10,13 +10,17 @@ const StyledArticleAuthor = styled.div`
   font-size: 1rem;
 `;
 
+const selectText = (selectedText) => {
+  console.log(selectedText)
+}
+
 function ArticleTextSelect({article, resetArticle}) {
   console.log(article)
   return (
     <div>
     <div>{article.title}</div>
 <StyledArticleAuthor>by {article.author}</StyledArticleAuthor>
-    <TextSelector article={article}/>
+    <TextSelector selectText={selectText} article={article}/>
     <Button onClick={resetArticle}>Return to articles</Button>
     </div>
   )
--- src/components/TextSelector.js
+++ src/components/TextSelector.js
@@ -7,7 +7,7 @@ const useStyles = makeStyles({
   width: '100%',
   height: '100%', 
   boxSizing: 'border-box',
-  fontSize: '1.4rem'
+  fontSize: '1rem'
   }
 })
 
