5d713e638f70506de6fc6d8acd037818b299eab0
Faktor_10 <oreillyross@gmail.com>
2020-03-23 14:26:04 +0100

    check prettier

--- src/tables/ScenarioTable.js
+++ src/tables/ScenarioTable.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 function ScenarioTable() {
-  return <div>This is the table</div>;
+  return <div>Scenarios</div>;
 }
 
 export default ScenarioTable;
