e5230de5363d2dd55720172cc185b8129f01c87d
Faktor_10 <oreillyross@gmail.com>
2020-03-24 22:42:42 +0100

    add form info

--- src/forms/ScenarioForm.js
+++ src/forms/ScenarioForm.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+function ScenarioForm() {
+  return (
+    <div>
+      Scenario Form
+      <Button
+        style={{ margin: "0 2rem" }}
+        onClick={() => {
+          navigate("/forms/newsource");
+        }}
+        basic
+        color="blue"
+      >
+        Add a Source
+      </Button>
+    </div>
+  );
+}
