f66bd3708fe10938f713aa2ac467cc355fe412d4
Faktor_10 <oreillyross@gmail.com>
2020-03-26 16:00:58 +0100

    fix hyperlink

--- src/views/Tag.js
+++ src/views/Tag.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { GET_TAG } from "../queries";
 import { useQuery } from "@apollo/react-hooks";
-import { Paper } from "@material-ui/core";
+import { Paper, Card, CardHeader, CardContent } from "@material-ui/core";
 function Tag({ id }) {
   const { data, loading } = useQuery(GET_TAG, {
     variables: {
@@ -12,7 +12,20 @@ function Tag({ id }) {
   if (data) {
     return (
       <div>
-        <Paper>{data.Tags[0].name}</Paper>
+        <Paper>
+          <Card variant="outlined">
+            <CardHeader title={data.Tags[0].name} />
+
+            <CardContent>
+              Is currently linked to{" "}
+              <a href={`/events/bytag/${data.Tags[0].id}`}>
+                {" "}
+                {data.Tags[0].Event_Tags_aggregate.aggregate.count}{" "}
+              </a>
+              Events.{" "}
+            </CardContent>
+          </Card>
+        </Paper>
       </div>
     );
   }
