4223efa1337aad38ea0916a25385838f5acc41b2
Faktor_10 <oreillyross@gmail.com>
2020-04-29 09:37:02 +0200

    Test the entries returned from Event
    This is using Object.entries to iterate over the Event object and see
    which properties come back. Nice easy way to get al the key value pairs
    and display them in web browser

--- src/views/Event.js
+++ src/views/Event.js
@@ -7,7 +7,17 @@ function Event({ id }) {
   const { data, loading } = useQuery(GET_EVENT_BY_ID, { variables: { id } });
   if (loading) return <Loading message="getting event..." />;
   if (data) {
-    return <div>{id}</div>;
+    console.log(data.Events[0]);
+    return (
+      <div>
+        <div>The ID taken from url is: {id}</div>
+        {Object.entries(data.Events[0]).map(keyVal => (
+          <div>
+            {keyVal[0]} : {keyVal[1]}{" "}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
