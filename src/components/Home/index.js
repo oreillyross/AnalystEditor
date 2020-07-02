2c5239132ee9f0b76ef50c17aae4afca3f2b17d8
Faktor_10 <oreillyross@gmail.com>
2020-07-02 20:35:37 +0200

    here we go

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,8 @@
 import React from "react";
+import Dashboard from "../../Dashboard";
 
 function Home() {
-  return (
-    <div>
-      <h1>This is the logged in page</h1>
-      <p>
-        The idea here is to show the logged in users favourite scenarios (come
-        up with better word for this) and to graphically display by default one
-        week predictions, then the use can change this to one omnth, one year,
-        or even the other way to one day. It is a kindof dashboard where the
-        user can easily see a list of all thier scenarios, by lcikcing on each
-        one the user is taken to another view where they can see the scenario
-        with the linked indicators, tags, etc. but also they can see how it has
-        changed over time.
-      </p>
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default Home;
