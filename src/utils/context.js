4ba43c37771b73b953f70999dfcff2cfdc5f83ae
Faktor_10 <oreillyross@gmail.com>
2020-07-16 13:32:48 +0200

    context

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 const AuthAppContext = React.createContext();
-const prj = { prjId: "12345566" };
-const AuthAppProvider = ({ children }) => (
-  <AuthAppContext.Provider value={prj}>{children}</AuthAppContext.Provider>
-);
+
+let prj = {};
+
+const AuthAppProvider = ({ children }) => {
+  const [prjState, setprjState] = React.useState({});
+
+  React.useEffect(() => {
+    //TODO use a graphql call to hasura to get all details you need based on logged in user and / or the project they are viewing
+    setprjState({ project_id: "ec214cf2-ba69-4d70-9bb6-43c91ad2fda7" });
+  }, []);
+
+  return (
+    <AuthAppContext.Provider value={prjState}>
+      {children}
+    </AuthAppContext.Provider>
+  );
+};
 export { AuthAppProvider, AuthAppContext };
