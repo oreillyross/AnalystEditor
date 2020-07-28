2fc3811938163470572a169d7ffbdecf1eadc5d9
Faktor_10 <oreillyross@gmail.com>
2020-07-28 23:09:30 +0200

    stuff

--- src/tables/ArticleTable.js
+++ src/tables/ArticleTable.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Button } from "reactstrap";
 import Moment from "react-moment";
 import HorizonTable from "../components/HorizonTable";
+import { format } from "date-fns";
+import { Link } from "@reach/router";
+import * as ROUTES from "../constants/routes";
 
 function getData(articles) {
   const rows = articles.map(article => ({
     title: article.title,
     published: article.published,
-    status: article.status
+    status: article.status,
+    url: article.url
   }));
   return rows;
 }
@@ -15,10 +19,18 @@ function getData(articles) {
 function getColumns() {
   const columns = [
     { Header: "Status", accessor: "status" },
-    { Header: "Published", accessor: "published" },
+    {
+      Header: "Published",
+      accessor: "published",
+      Cell: props => format(new Date(props.value), "PPP")
+    },
     {
       Header: "Title",
-      accessor: "title"
+      accessor: "title",
+      Cell: props => {
+        console.log(props);
+        return <Link to={ROUTES.LANDING}>{props.value}</Link>;
+      }
     }
   ];
   return columns;
