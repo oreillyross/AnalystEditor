878700ce4248eb40417604b1b84c87ded91508af
Ross O'Reilly <oreillyross@gmail.com>
2021-04-13 17:33:10 +0200

    Comment out code to start client side first

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "react-datepicker/dist/react-datepicker.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-bootstrap-typeahead/css/Typeahead.css";
-import Firebase, { FirebaseContext } from "./components/Firebase";
-import ApolloClient from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import httplink from "./http-link";
-import errorlink from "./error-link";
-//import { alink } from "./alink.js";
+// // import "react-datepicker/dist/react-datepicker.css";
+// // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// // import "bootstrap/dist/css/bootstrap.min.css";
+// // import "react-bootstrap-typeahead/css/Typeahead.css";
+// // import Firebase, { FirebaseContext } from "./components/Firebase";
+// // import ApolloClient from "apollo-client";
+// // import { ApolloLink } from "apollo-link";
+// // import { ApolloProvider } from "@apollo/react-hooks";
+// // import { InMemoryCache } from "apollo-cache-inmemory";
+// // import httplink from "./http-link";
+// // import errorlink from "./error-link";
+// //import { alink } from "./alink.js";
 
-const link = new ApolloLink.from([errorlink, httplink]);
+// const link = new ApolloLink.from([errorlink, httplink]);
 
 // the Apollo cache is set up automatically
 // const client = new ApolloClient({
@@ -26,9 +26,4 @@ const link = new ApolloLink.from([errorlink, httplink]);
 // });
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
- 
-      <App />
-  ,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
